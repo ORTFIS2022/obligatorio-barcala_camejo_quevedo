1715a98bfcd0ae64d8b04d1b27137c1acce4ecc1
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2022-11-15 17:40:16 -0300

    Añadimos Notificaciones

--- interfaz/src/index.js
+++ interfaz/src/index.js
@@ -6,6 +6,7 @@ import {MDCTopAppBar} from '@material/top-app-bar';
 import {MDCDrawer} from "@material/drawer";
 import {MDCList} from "@material/list";
 
+//LA LISTA DESPLEGABLE
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
@@ -17,14 +18,26 @@ topAppBar.listen('MDCTopAppBar:nav', () => {
 });
 
 
+//BOTON/OCULTAR
+document.getElementById("botonB").addEventListener("click",ocultarTodo);
 
-document.getElementById("botonB").addEventListener("click",test);
 
-
-function test(){
+//FUNCION OCULTAR BOTONES
+function ocultar(){
+  document.getElementById('Notificaciones').style.visibility = 'hidden';
+}
+//FUNCION OCULTAR TODO
+function ocultarTodo(){
+  document.getElementById('botonA').style.visibility = 'hidden';
   document.getElementById('botonB').style.visibility = 'hidden';
+  document.getElementById('botonC').style.visibility = 'hidden';
+  document.getElementById('botonD').style.visibility = 'hidden';
+
 }
 
+//BOTONES NOTIFICACIONES
+document.getElementById("botonNotifi").addEventListener("click",ocultar);
+document.getElementById("botonNotifi2").addEventListener("click",ocultar);
 
 
 
